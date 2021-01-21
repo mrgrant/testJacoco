90fec65c732208181818a7f6a20d3a665ae76443
chenxw1 <chenxw1@getui.com>
2021-01-21 16:03:56 +0800

    [feature]: ADD

---
+++ src/main/java/com/test/demo/Plus.java
@@ -0,0 +1,11 @@
+package com.test.demo;
+
+/**
+ * @Author: Chen Xinwei
+ * @Date: 2021/1/21 3:57 下午
+ */
+public class Plus {
+    public static int add(Integer a, Integer b) {
+        return a + b;
+    }
+}
--- src/test/java/com/test/demo/DemoApplicationTests.java
+++ src/test/java/com/test/demo/DemoApplicationTests.java
@@ -2,12 +2,19 @@ package com.test.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void TestAdd() {
+        assertEquals(2, Plus.add(1, 1));
+        assertEquals(3, Plus.add(1, 2));
     }
 
 }
