69a50ade918f3fd873f699e9e0f5ab438e6c9c54
chenxw1 <chenxw1@getui.com>
2021-01-22 16:42:38 +0800

    [feature]: change utils add

--- src/main/java/com/test/demo/utils/Plus.java
+++ src/main/java/com/test/demo/utils/Plus.java
@@ -6,7 +6,7 @@ package com.test.demo.utils;
  */
 public class Plus {
     public static int add(Integer a, Integer b) {
-        return a + b;
+        return a + b + a;
     }
 
     public static long fact(long n) {
